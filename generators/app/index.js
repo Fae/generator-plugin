f56e3edf6168bfd6182c7e141b72939ebc6000ed
Chad Engler <englercj@live.com>
2016-08-19 18:39:25 -0700

    implement template files for a plugin

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,16 +9,28 @@ module.exports = yeoman.Base.extend({
     prompting()
     {
         // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the badass ' + chalk.red('generator-plugin') + ' generator!'
-        ));
+        this.log(yosay(`${chalk.red('Fae Plugin')} generator:`));
 
-        const prompts = [{
-            type: 'confirm',
-            name: 'someAnswer',
-            message: 'Would you like to enable this option?',
-            default: true,
-        }];
+        const prompts = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter the plugin\'s name:',
+            },
+            {
+                type: 'confirm',
+                name: 'isOfficial',
+                message: 'Is this an official plugin?',
+                default: false,
+            },
+            {
+                type: 'input',
+                name: 'githubUrl',
+                message: 'Enter the URL for the github page:',
+                default: 'https://github.com/Fae/fae',
+                when: (props) => !props.isOfficial,
+            },
+        ];
 
         return this.prompt(prompts).then((props) =>
         {
@@ -29,9 +41,23 @@ module.exports = yeoman.Base.extend({
 
     writing()
     {
+        // root files
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json'),
+            this.props
+        );
+
+        // src files
         this.fs.copy(
-            this.templatePath('dummyfile.txt'),
-            this.destinationPath('dummyfile.txt')
+            this.templatePath('src/index.js'),
+            this.destinationPath('src/index.js')
+        );
+
+        // test files
+        this.fs.copy(
+            this.templatePath('test/_.eslintrc.json'),
+            this.destinationPath('test/.eslintrc.json')
         );
     },
 
